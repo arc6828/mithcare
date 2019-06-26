d668f63a96fe3181c815364f74b2d7615b7552c9
Chavalit Koweerawong <chavalit.kow@gmail.com>
2019-06-26 23:40:25 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'csincube_wmithcare' );
+define( 'DB_NAME', 'mithcare' );
 
 /** MySQL database username */
-define( 'DB_USER', 'csincube_wmithcare' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
